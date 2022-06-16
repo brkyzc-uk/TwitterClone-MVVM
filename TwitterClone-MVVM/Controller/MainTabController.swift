0bfd91786284a2530b34985c19e8d618307a27b4
brkyzc-uk <burak.yazici@anadolu.edu.tr>
2022-06-16 14:11:11 +0300

    New tweet button design created

--- TwitterClone-MVVM/Controller/MainTabController.swift
+++ TwitterClone-MVVM/Controller/MainTabController.swift
@@ -13,10 +13,12 @@ class MainTabController: UITabBarController {
     let actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .white
-        button.backgroundColor = .blue
+        button.backgroundColor = .twitterBlue
         button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
         return button
     }()
+    
     
     // MARK: - Lifecycle
 
@@ -25,17 +27,27 @@ class MainTabController: UITabBarController {
 
         configureViewControllers()
         configureUI()
+        
+    }
+    
+    @objc func actionButtonTapped() {
+        print("button tapped")
     }
     
     // MARK: - Helpers
     
     func configureUI() {
+    
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithDefaultBackground()
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithDefaultBackground()
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        
         view.addSubview(actionButton)
-        actionButton.translatesAutoresizingMaskIntoConstraints = false
-        actionButton.heightAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.widthAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64).isActive = true
-        actionButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
         actionButton.layer.cornerRadius = 56 / 2
         
     }
