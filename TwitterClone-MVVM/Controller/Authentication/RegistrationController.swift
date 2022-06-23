2406a13137fa9f9fd0b07d23255cc4238640d662
brkyzc-uk <burak.yazici@anadolu.edu.tr>
2022-06-23 18:59:56 +0300

    Setup Firebase
    Auth and upload user data

--- TwitterClone-MVVM/Controller/Authentication/RegistrationController.swift
+++ TwitterClone-MVVM/Controller/Authentication/RegistrationController.swift
@@ -7,12 +7,13 @@
 
 import Foundation
 import UIKit
-
+import Firebase
 
 class RegistrationController: UIViewController {
     // MARK: - Properties
     
     private let imagePicker = UIImagePickerController()
+    private var profileImage: UIImage?
     
     private let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
@@ -98,7 +99,28 @@ class RegistrationController: UIViewController {
     }
     
     @objc func handleSignup() {
-        print("Sign up...")
+        guard let profileImage = profileImage else {
+            print("DEBUG: Please select a profile image...")
+            return
+        }
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let fullname = fullnameTextField.text else { return }
+        guard let username = usernameTextField.text else { return }
+       
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Error is \(error.localizedDescription)")
+                return
+            }
+            guard let uid = result?.user.uid else { return }
+            let values = ["email": email, "username": username, "fullname": fullname]
+            let ref = Database.database().reference().child("users").child(uid)
+            
+            ref.updateChildValues(values) { error, ref in
+                print("DEBUG: Successfully updated user information...")
+            }
+        }
     }
     
     @objc func handleAddProfilePhoto() {
@@ -142,6 +164,7 @@ class RegistrationController: UIViewController {
 extension RegistrationController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let profileImage = info[.editedImage] as? UIImage else { return }
+        self.profileImage = profileImage
         
         plusPhotoButton.layer.cornerRadius = 128 / 2
         plusPhotoButton.layer.masksToBounds = true
